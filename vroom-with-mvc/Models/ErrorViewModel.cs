822680d18a3c57b9f905aab048293015a1f6db92
engineerapurba96 <111281857+engineerapurba96@users.noreply.github.com>
2023-11-09 16:50:54 +0530

    Initial commit of dot net project creation

---
+++ vroom-with-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vroom_with_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
